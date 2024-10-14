d1a2b686f2513eaf170747ccab87e5f453952152
Ruan Oliveira <ruanwesley93@gmail.com>
2024-10-14 13:20:34 -0300

    new commit

---
+++ index.js
@@ -0,0 +1,105 @@
+// Todos os botões da sessão Informations
+const btn = document.querySelectorAll('.section-info a, .sectionOutubroRosa a')
+const btnHeader = document.querySelectorAll('.header-topo a')
+console.log(btn, btnHeader)
+
+// Sections Campanhas/Doações/Sobre/Contato
+let campanha = document.getElementById('campanhas')
+let doacoes = document.getElementById('doacoes')
+
+// Sections Informações/Exames/Equipe Médica/Tratamento
+let information = document.getElementById('informations')
+let equipeMedica = document.getElementById('section-equipe-medica')
+let exames = document.getElementById('section-exames')
+let tratamento = document.getElementById('section-tratamento')
+
+// Botão Campanhas
+btnHeader[1].addEventListener('click', () => {
+    campanha.style.display = 'flex'
+})
+
+btnHeader[2].addEventListener('click', () => {
+    doacoes.style.display = 'grid'
+})
+
+
+// Botão Saiba Mais
+btn[0].addEventListener('click', () => {
+    information.style.display = 'flex'
+})
+
+// Botão Exames
+btn[1].addEventListener('click', () => {
+    exames.style.display = 'flex'
+    equipeMedica.style.display = 'none'
+    tratamento.style.display = 'none'
+
+    if (exames.style.display == 'flex') {
+        btn[1].style.background = 'red'
+        btn[1].style.border = '1px solid red'
+        btn[1].style.color = 'white'
+
+        btn[2].style.backgroundColor = '#29d0f2'
+        btn[2].style.border = '1px solid #29d0f2'
+        btn[2].style.color = 'black'
+        
+        btn[3].style.backgroundColor = '#29d0f2'
+        btn[3].style.border = '1px solid #29d0f2'
+        btn[3].style.color = 'black'
+
+    }
+})
+
+// Botão Equipe Médica
+btn[2].addEventListener('click', () => {
+    equipeMedica.style.display = 'flex'
+    exames.style.display = 'none'
+    tratamento.style.display = 'none'
+
+    if (equipeMedica.style.display == 'flex') {
+        btn[2].style.background = 'red'
+        btn[2].style.border = '1px solid red'
+        btn[2].style.color = 'white'
+
+        btn[1].style.backgroundColor = '#29d0f2'
+        btn[1].style.border = '1px solid #29d0f2'
+        btn[1].style.color = 'black'
+        
+        btn[3].style.backgroundColor = '#29d0f2'
+        btn[3].style.border = '1px solid #29d0f2'
+        btn[3].style.color = 'black'
+
+    }
+})
+
+// Botão Tratamento
+btn[3].addEventListener('click', () => {
+    tratamento.style.display = 'flex'
+    equipeMedica.style.display = 'none'
+    exames.style.display = 'none'
+
+    if (tratamento.style.display == 'flex') {
+        btn[3].style.background = 'red'
+        btn[3].style.border = '1px solid red'
+        btn[3].style.color = 'white'
+
+        btn[1].style.backgroundColor = '#29d0f2'
+        btn[1].style.border = '1px solid #29d0f2'
+        btn[1].style.color = 'black'
+        
+        btn[2].style.backgroundColor = '#29d0f2'
+        btn[2].style.border = '1px solid #29d0f2'
+        btn[2].style.color = 'black'
+    }
+})
+
+const myAnimation = document.querySelectorAll('.carousel, .redesSociais')
+
+myAnimation.forEach((e) => {
+    setTimeout(() => {
+        document.querySelector('.section-texto').style.display = 'flex'
+        document.querySelector('.redesSociais').style.display = 'flex'
+        e.classList.add('textAnimation')
+
+    }, 3000)
+})
